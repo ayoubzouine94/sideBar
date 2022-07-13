c7c0517d6a6e5002304b1ae4b10254127f9eec30
ayoubzouine94 <ayoubzouine94@gmail.com>
2022-07-13 03:30:05 +0100

    side bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import SideBar from './components/SideBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DashbBoard from './pages/DashbBoard';
+import About from './pages/About';
+import Analytics from './pages/Analytics';
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import Comment from './pages/Comment'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <BrowserRouter>
+      <SideBar>
+          <Routes>
+                <Route path='/' element={<DashbBoard />} />
+                <Route path='/dashboard' element={<DashbBoard />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/comment' element={<Comment />} />
+                <Route path='/analytics' element={<Analytics />} />
+                <Route path='/product' element={<Product />} />
+                <Route path='/productList' element={<ProductList />} />
+                <Route path='/ali' element={<ProductList />} />
+          </Routes>
+      </SideBar>
+        
+        
+      </BrowserRouter>
+      
+
+    
+    
   );
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,67 @@
+import React,{useState} from 'react';
+import {FaTh,FaBars,FaShoppingCart ,FaUserAlt,FaRegChartBar,FaThList,FaCommentAlt} from 'react-icons/fa';
+import {NavLink} from 'react-router-dom';
+
+const SideBar = ( {children}) => {
+    const[isOpen, setIsopen] = useState(false);
+    const toggle = () =>setIsopen(!isOpen)
+    const menuItems=[
+        {
+            path:'/',
+            name:'Dashboard',
+            icon:<FaTh />,
+        },
+       
+        {
+            path:'/about',
+            name:'About',
+            icon:<FaUserAlt />
+        },
+        {
+            path:'/analytics',
+            name:'analytics',
+            icon:<FaRegChartBar />
+        },
+        {
+            path:'/comment',
+            name:'Comment',
+            icon:<FaCommentAlt/>
+        },
+        {
+            path:'/product',
+            name:'Product',
+            icon:<FaShoppingCart/>
+        },
+        {
+            path:'/productList',
+            name:'ProductList',
+            icon:<FaThList/>
+        },
+        
+    ]
+  return (
+    <div className='container'>
+        <div style={{width: isOpen ? "250px" :"50px"}} className="sidebar">
+            <div className="top_section">
+                <h1 style={{display:isOpen ? "block" : "none"}} className='logo'>Logo</h1>
+                <div className="bars" style={{marginLeft:isOpen ? "50px" : "0px"}}>
+                   <FaBars onClick={toggle} />   
+                </div>
+            </div>
+            {
+                menuItems.map((item , index) =>(
+                 <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                    <div className="icon">{item.icon}</div>
+                    <div style={{display:isOpen ? "block" : "none"}}  className="link_text">{item.name}</div>
+                 </NavLink>
+                )
+                
+                )
+            }
+        </div>
+        <main>{children}</main>
+    </div>
+  )
+}
+
+export default SideBar
